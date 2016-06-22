247916fae67dcaba2d663b1bf3a84e433dc61007
Geiko <k.i.geiko@gmail.com>
2016-06-22 11:57:41 +0300

    back fix

--- 12_warrior/js/buttonHendlers.js
+++ 12_warrior/js/buttonHendlers.js
@@ -1,35 +1,36 @@
+;$(function(){
 
-
-		$("#born").click(function(){
-			warrior1.born();
-		});
-		$("#takeSword").click(function(){
-			warrior1.takeSword();
-		});
-		$("#go").click(function(){
-			warrior1.go();
-		});
-		$("#back").click(function(){
-			warrior1.back();
-		});
-		$("#shield").click(function(){
-			warrior1.shield();
-		});
-		$("#hit").click(function(){
-			warrior1.hit();
-		});
-		$("#upHit").click(function(){
-			warrior1.upHit();
-		});			
-		
-		// document.getElementById("hit").addEventListener("click", warrior.hit);
-		// document.getElementById("upHit").addEventListener("click", warrior.upHit);
+	var warrior = new Warrior();
 
 
 
+	$("#born").click(function(){
+		warrior.born();
+	});
+	$("#takeSword").click(function(){
+		warrior.takeSword();
+	});
+	$("#back").click(function(){
+		warrior.back();
+	});
+	$("#go").click(function(){
+		warrior.go();
+	});
+	$("#shield").click(function(){
+		warrior.shield();
+	});
+	$("#hit").click(function(){
+		warrior.hit();
+	});
+	$("#upHit").click(function(){
+		warrior.upHit();
+	});			
+	
+	// document.getElementById("hit").addEventListener("click", warrior.hit);
+	// document.getElementById("upHit").addEventListener("click", warrior.upHit);
 
-		var warrior1 = new Warrior();
-		//var warrior2 = new Warrior();
 
-		warrior1.born();
-		//warrior2.born();
+	warrior.born();
+	warrior.born();
+	warrior.born();
+})
--- 12_warrior/js/wariorObject.js
+++ 12_warrior/js/wariorObject.js
@@ -1,232 +1,145 @@
-		// var warrior = {
+	var Warrior = function( ){
 
-		// 	go: function(){
+		this.picWidth = 100;
+		this.picHeight = 100;
 
-		// 		var set = $("div.warrior").offset();
-		// 		set.left += 50;
-		// 		$("div.warrior").offset(set);
-		// 	},
-
-		// 	back: function(){
-
-		// 		var set = $("div.warrior").offset();
-		// 		set.left -= 50;
-		// 		$("div.warrior").offset(set);
-		// 	},
-
-		// 	act: function (url){
-		// 		//$(".warrior").html("<img src=\"" + url + "\" alt=\"warrior\" width=\"150\" height=\"150\">" );
-		// 		var temp = document.getElementsByClassName("warrior")[0];
-		// 		temp.innerHTML = "<img src=\"" + url + "\" alt=\"warrior\" width=\"200\" height=\"200\">";
-		// 	},
-
-		// 	born: function (){
-		// 		warrior.act("img/warrior.png");
-		// 	},
-
-		// 	takeSword: function(){
-		// 		warrior.act("img/takeSword.png");
-		// 	},
-
-		// 	shield: function(){
-
-		// 		warrior.act("img/shield.png");
-
-		// 		setTimeout(function(){
-		// 			warrior.act("img/shield2.png");
-		// 		}, 300);
-
-		// 		setTimeout(function(){
-		// 			warrior.act("img/shield.png");
-		// 		}, 600);
-
-		// 		setTimeout(function(){
-		// 			warrior.act("img/takeSword.png");
-		// 		}, 900);
-		// 	},
-
-		// 	hit: function(){
-
-		// 		warrior.go();
-		// 		warrior.act("img/hit.png");
-
-		// 		setTimeout(function(){
-		// 			warrior.act("img/takeSword.png");
-		// 		}, 300);
-
-		// 		setTimeout(function(){
-		// 			warrior.back();
-		// 			warrior.shield();
-		// 		}, 600);
-		// 	},
-
-		// 	upHit: function(){
-
-		// 		warrior.act("img/upHit1.png");
-
-		// 		setTimeout(function(){
-		// 			warrior.act("img/upHit2.png");
-		// 		}, 200);
-
-		// 		setTimeout(function(){
-		// 			warrior.go();
-		// 			warrior.act("img/upHit3.png");
-		// 		}, 400);
-
-		// 		setTimeout(function(){
-		// 			warrior.go();
-		// 			warrior.act("img/upHit4.png");
-		// 		}, 600);			
-
-		// 		setTimeout(function(){
-		// 			warrior.back();
-		// 			warrior.act("img/takeSword.png");
-		// 		}, 800);			
-
-		// 		setTimeout(function(){
-		// 			warrior.back();
-		// 			warrior.shield();
-		// 		}, 1000);
-		// 	}
-		// };
+		this.step = 50;
+		this.timeInterval = 300;
 
 
 
-		var Warrior = function( ){
+		this.act = function (url, isBorn){
+			// if(isBorn){
+			// 	$(".Warrior:first-child").html("<img src=\"" + url + "\" alt=\"Warrior\" width=\"150\" height=\"150\">" );
+			// }
+			// else{
+			// 	$(".Warrior:last-child").html("<img src=\"" + url + "\" alt=\"Warrior\" width=\"150\" height=\"150\">" );
+			// }
 
-			this.picWidth = 100;
-			this.picHeight = 100;
-
-			this.step = 50;
-			this.timeInterval = 300;
-
-
-
-			this.act = function (url, isBorn){
-				// if(isBorn){
-				// 	$(".Warrior:first-child").html("<img src=\"" + url + "\" alt=\"Warrior\" width=\"150\" height=\"150\">" );
-				// }
-				// else{
-				// 	$(".Warrior:last-child").html("<img src=\"" + url + "\" alt=\"Warrior\" width=\"150\" height=\"150\">" );
-				// }
-
-				if(isBorn){
-				var temp = document.getElementsByClassName("Warrior")[0];
+			if(isBorn){
+			var temp = document.getElementsByClassName("Warrior")[0];
+			temp.innerHTML = "<img src=\"" + url + 
+				"\" alt=\"Warrior\" width=\""+ this.picWidth +"\" height=\""+ this.picHeight +"\">";
+			}
+			else{
+				var temp = document.getElementsByClassName("Warrior");
+				temp = temp[temp.length-1];
 				temp.innerHTML = "<img src=\"" + url + 
 					"\" alt=\"Warrior\" width=\""+ this.picWidth +"\" height=\""+ this.picHeight +"\">";
-				}
-				else{
-					var temp = document.getElementsByClassName("Warrior");
-					temp = temp[temp.length-1];
-					temp.innerHTML = "<img src=\"" + url + 
-						"\" alt=\"Warrior\" width=\""+ this.picWidth +"\" height=\""+ this.picHeight +"\">";
-				}
-			};
+			}
+		};
 
 
 
-			this.born = function (){
+		this.born = function (){
 
-				var newDiv = document.createElement('div');
-				newDiv.className = 'Warrior';
+			var newDiv = document.createElement('div');
+			newDiv.className = 'Warrior';
 
-				var fieldDiv = document.getElementsByClassName('field')[0];
-				fieldDiv.insertBefore(newDiv, fieldDiv.firstChild);
+			var fieldDiv = document.getElementsByClassName('field')[0];
+			fieldDiv.insertBefore(newDiv, fieldDiv.firstChild);
 
-				this.act("img/warrior.png", true);
-			};
-
-
-
-			this.go = function(){
-
-				var set = $("div.Warrior:last-child").offset();
-				set.left += this.step;
-				$("div.Warrior:last-child").offset(set);
-			};
+			this.act("img/warrior.png", true);
+		};
 
 
 
-			this.back = function(){
+		this.go = function(){
 
-				var set = $("div.Warrior:last-child").offset();
+			var set = $("div.Warrior:last-child").offset();
+			set.left += this.step;
+			$("div.Warrior:last-child").offset(set);
+		};
+
+
+
+		this.back = function(){
+
+			var set = $("div.Warrior:last-child").offset();
+
+			//var allWarriors = document.getElementsByClassName('Warrior');
+			var penultWarriorIndex = document.getElementsByClassName('Warrior').length - 2;
+			var penultSet = $("div.Warrior").eq(penultWarriorIndex).offset();
+
+			if( set.left >= penultSet.left + this.picWidth){
 				set.left -= this.step;				
 				$("div.Warrior:last-child").offset(set);
-			};
-
-
-
-			this.takeSword = function(){
-				this.act("img/takeSword.png");
-			};
-
-
-
-			this.shield = function(){
-
-				this.act("img/shield.png");				
-				var WarriorThis = this;					//	 this;	
-
-				setTimeout(function (){ 
-					WarriorThis.act("img/shield2.png"); 
-				}, this.timeInterval);
-
-				setTimeout(function(){
-					WarriorThis.act("img/shield.png");
-				}, this.timeInterval * 2);
-
-				setTimeout(function(){
-					WarriorThis.act("img/takeSword.png");
-				}, this.timeInterval * 3);
-			};
-
-
-
-			this.hit = function(){
-
-				this.go();
-				this.act("img/hit.png");
-				var WarriorThis = this;					//	 this;	
-
-				setTimeout(function(){
-					WarriorThis.act("img/takeSword.png");
-				}, this.timeInterval);
-
-				setTimeout(function(){
-					WarriorThis.back();
-					WarriorThis.shield();
-				}, this.timeInterval * 2);
-			};
-
-
-
-			this.upHit = function(){
-
-				this.act("img/upHit1.png");
-				var WarriorThis = this;					//	 this;	
-
-				setTimeout(function(){
-					WarriorThis.act("img/upHit2.png");
-				}, this.timeInterval/3*2);
-
-				setTimeout(function(){
-					WarriorThis.go();
-					WarriorThis.act("img/upHit3.png");
-				}, this.timeInterval/3*2 * 2);
-
-				setTimeout(function(){
-					WarriorThis.go();
-					WarriorThis.act("img/upHit4.png");
-				}, this.timeInterval/3*2 * 3);			
-
-				setTimeout(function(){
-					WarriorThis.back();
-					WarriorThis.act("img/takeSword.png");
-				}, this.timeInterval/3*2 * 4);			
-
-				setTimeout(function(){
-					WarriorThis.back();
-					WarriorThis.shield();
-				}, this.timeInterval/3*2 * 5);
 			}
+		};
+
+
+
+		this.takeSword = function(){
+			this.act("img/takeSword.png");
+		};
+
+
+
+		this.shield = function(){
+
+			this.act("img/shield.png");				
+			var WarriorThis = this;					//	 this;	
+
+			setTimeout(function (){ 
+				WarriorThis.act("img/shield2.png"); 
+			}, this.timeInterval);
+
+			setTimeout(function(){
+				WarriorThis.act("img/shield.png");
+			}, this.timeInterval * 2);
+
+			setTimeout(function(){
+				WarriorThis.act("img/takeSword.png");
+			}, this.timeInterval * 3);
+		};
+
+
+
+		this.hit = function(){
+
+			this.go();
+			this.act("img/hit.png");
+			var WarriorThis = this;					//	 this;	
+
+			setTimeout(function(){
+				WarriorThis.act("img/takeSword.png");
+			}, this.timeInterval);
+
+			setTimeout(function(){
+				WarriorThis.back();
+				WarriorThis.shield();
+			}, this.timeInterval * 2);
+		};
+
+
+
+		this.upHit = function(){
+
+			this.act("img/upHit1.png");
+			var WarriorThis = this;					//	 this;	
+
+			setTimeout(function(){
+				WarriorThis.act("img/upHit2.png");
+			}, this.timeInterval/3*2);
+
+			setTimeout(function(){
+				WarriorThis.go();
+				WarriorThis.act("img/upHit3.png");
+			}, this.timeInterval/3*2 * 2);
+
+			setTimeout(function(){
+				WarriorThis.go();
+				WarriorThis.act("img/upHit4.png");
+			}, this.timeInterval/3*2 * 3);			
+
+			setTimeout(function(){
+				WarriorThis.back();
+				WarriorThis.act("img/takeSword.png");
+			}, this.timeInterval/3*2 * 4);			
+
+			setTimeout(function(){
+				WarriorThis.back();
+				WarriorThis.shield();
+			}, this.timeInterval/3*2 * 5);
 		}
+	};
